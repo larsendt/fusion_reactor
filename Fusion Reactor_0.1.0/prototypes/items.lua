8d240b731652a92e23fdcfe321b735ee5d5beae8
Dane Larsen <dane.t.larsen@gmail.com>
2016-03-17 18:24:33 -0600

    fusion reactor item

--- Fusion Reactor_0.1.0/prototypes/items.lua
+++ Fusion Reactor_0.1.0/prototypes/items.lua
@@ -1,3 +1,13 @@
 data:extend(
 {
+    {
+        type = "item",
+        name = "deuterium-fusion-reactor",
+        icon = "__Fusion Reactor__/graphics/icons/deuterium-fusion-reactor.png",
+        flags = {"goes-to-quickbar"},
+        subgroup = "production-machine",
+        order = "e[deuterium-fusion-reactor]",
+        place_result = "deuterium-fusion-reactor",
+        stack_size = 10
+    }
 })
